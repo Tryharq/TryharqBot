853cf22356b4e009c5e91ec71f6342e64f370b32
Tryharq <48394988+Tryharq@users.noreply.github.com>
2019-03-09 23:55:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NTUzNjIyMjg5MzQ2OTg1OTk0.D2XVUA.QfkxWQbOs26ih0bPS23c7U_lFao);
