a8e6cfdd1f8852d38e67791a9dac468e6234c835
Tryharq <48394988+Tryharq@users.noreply.github.com>
2019-03-10 12:18:32 +0100

    Update tryharqbot.js

--- tryharqbot.js
+++ tryharqbot.js
@@ -3,8 +3,26 @@ const client = new Discord.Client();
 
 bot.on('ready', () => {
     console.log("Turning On Bot")
-    bot.user.setActivity("New Users", {type: 2}
-                                 
+    bot.user.setActivity("New Users", {type: 2});
+});
+
+bot.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome-leave');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':bust_in_silhouette: | name : ', `${member}`)
+        .addField(':microphone2: | Welcome!', `Welcome to the server, ${member}`)
+        .addField(':id: | User :', "**[" + `${member.id}` + "]**")
+        .addField(':family_mwgb: | Your are the member', `${member.guild.memberCount}`)
+        .addField("Name", `<@` + `${member.id}` + `>`, true)
+        .addField('Server', `${member.guild.name}`, true )
+        .setFooter(`**${member.guild.name}**`)
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
 });
 
 bot.on('guildMemberAdd', member => {
@@ -12,7 +30,6 @@ bot.on('guildMemberAdd', member => {
     console.log(`${member}`, "has joined" + `${member.guild.name}`)
 
 
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
