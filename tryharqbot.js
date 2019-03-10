e8fbdc4116ee8c723e2b2a5b0e5e8d7d49cba7fe
Tryharq <48394988+Tryharq@users.noreply.github.com>
2019-03-10 13:54:25 +0100

    Update tryharqbot.js

--- tryharqbot.js
+++ tryharqbot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity('tryharqbot')
 });
 
 client.on("GuildMemberAdd".function(message) {
