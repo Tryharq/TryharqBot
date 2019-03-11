fc4e4880402edf4c43471ebb780257732543e93f
Tryharq <48394988+Tryharq@users.noreply.github.com>
2019-03-11 12:47:43 +0100

    Update tryharqbot.js

--- tryharqbot.js
+++ tryharqbot.js
@@ -1,14 +1,14 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
- 
-client.on("ready", () => {
-  console.log("I am ready!");
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
- 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
   }
 });
- 
-client.login("BOT_TOKEN");
+
+client.login('token');
