e6b39b93d6cf1e795831021e0bf0ed5bd0d2b56a
Tryharq <48394988+Tryharq@users.noreply.github.com>
2019-03-11 14:41:29 +0100

    Update tryharqbot.js

--- tryharqbot.js
+++ tryharqbot.js
@@ -11,11 +11,5 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === '!staff') {
-    	message.reply('Onwer tryharq_& BASplayer');
-  	    
-    }
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
