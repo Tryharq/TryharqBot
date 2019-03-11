2fb4620ae3606be78c5b5c2c1f804f85ca6a0be4
Tryharq <48394988+Tryharq@users.noreply.github.com>
2019-03-11 12:38:56 +0100

    Update tryharqbot.js

--- tryharqbot.js
+++ tryharqbot.js
@@ -1,25 +1,14 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+ 
+client.on("ready", () => {
+  console.log("I am ready!");
 });
-
-client.on("GuildMemberAdd".function(message) {
-
-  let guild = message.guild;
-  let member = message;
-  let membercounter = client.users.size;
-          
-  const embed = new discord.RichEmed()
-  .setcolor(0xffffff)
-  .setTitle('Tryharq_ & Basplayer Community - welcome')
-  .setdescription('hallo ${member.user}, welkom op Tryharq_ & Basplayer Community.')
-  .setThumbnail(member.user.avatarURL)
-  .setfooter(footer)
-
-  member.guild.channels.find('name','welcome').send({ embed: embed }):
+ 
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
 });
-          
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+ 
+client.login("SuperSecretBotTokenHere");
