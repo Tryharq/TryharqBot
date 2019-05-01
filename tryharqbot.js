c956deb8b67e0e7abe0c599f4a0296f1d518e78b
Tryharq <48394988+Tryharq@users.noreply.github.com>
2019-05-01 19:40:58 +0200

    Update tryharqbot.js

--- tryharqbot.js
+++ tryharqbot.js
@@ -3,12 +3,12 @@ const client = new Discord.Client()
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
+ar bot = new Discord.Client();
 
-   }
-}) 
+bot.on("guildMemberAdd", member => {
+    let mem = member.guild
+    mem.defaultChannel.sendMessage(member.user + " welcome to the server!"); 
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
